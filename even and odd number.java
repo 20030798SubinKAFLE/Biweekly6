41490cf6df4685120b6e58a621388bef1f4adc08
20030798SubinKAFLE <20030798@students.koi.edu.au>
2025-12-07 19:36:42 +1100

    Add files via upload

---
+++ even and odd number.java
@@ -0,0 +1,22 @@
+package week5;
+import java.util.Scanner;
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner keyboard= new Scanner(System.in);
+		int number= 2;
+		System.out.println("enter any number:");
+		
+		
+		if(number%2==0) {
+			System.out.println("even number.");
+		}
+		else {
+				System.out.println("odd number.");
+		}
+	}
+		
+	
+	}
+
